792ee22d8276658ffdb2e742c5e6c16dfa2324c2
Krystie-W <tsukio.scoring@gmail.com>
2021-11-30 15:29:43 +1100

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  const bio = props.bio;
+  //I don't like how I've done this, but when I tried to do it as JS inline (in between curly brackets), the test wasn't passing for some reason.
+  if (!bio) return (
+    <div id="about">
+      <h2>About Me</h2>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github={props.linksgh} linkedin={props.linksli} />
+    </div>
+  )
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.linksgh} linkedin={props.linksli} />
     </div>
   );
 }
